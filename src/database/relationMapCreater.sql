51b3704e7f2df865d479e0ecdc39e2c1d7adf587
yajamon <yajamon.tatsuki@gmail.com>
2014-03-16 17:00:08 +0900

    edit database information
    auto increment が抜けている問題を修正

--- src/database/relationMapCreater.sql
+++ src/database/relationMapCreater.sql
@@ -2,8 +2,8 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 /* Drop Tables */
 
-DROP TABLE labels_subjects;
 DROP TABLE informations_subjects;
+DROP TABLE labels_subjects;
 DROP TABLE subjects;
 DROP TABLE informations_labels;
 DROP TABLE informations;
@@ -45,21 +45,6 @@ CREATE TABLE informations
 ) ENGINE = InnoDB COMMENT = '情報テーブル : 情報テーブル' DEFAULT CHARACTER SET utf8;
 
 
-CREATE TABLE labels_subjects
-(
-	id int unsigned NOT NULL UNIQUE COMMENT 'ラベル_主体連携id',
-	-- モノID
-	subject_id int unsigned NOT NULL COMMENT '主体id : モノID',
-	-- タグID
-	label_id int unsigned NOT NULL COMMENT 'ラベルid : タグID',
-	-- 作成日時
-	created datetime COMMENT '作成日時 : 作成日時',
-	-- 変更日時
-	modified datetime COMMENT '変更日時 : 変更日時',
-	PRIMARY KEY (id)
-) ENGINE = InnoDB COMMENT = 'ラベル_主体連携テーブル' DEFAULT CHARACTER SET utf8;
-
-
 CREATE TABLE informations_subjects
 (
 	-- 識別id
@@ -92,7 +77,7 @@ CREATE TABLE labels
 
 CREATE TABLE informations_labels
 (
-	id int unsigned NOT NULL UNIQUE COMMENT '情報_ラベルid',
+	id int unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT '情報_ラベルid',
 	-- 情報id
 	information_id int unsigned NOT NULL COMMENT '情報id : 情報id',
 	-- タグID
@@ -105,8 +90,31 @@ CREATE TABLE informations_labels
 ) ENGINE = InnoDB COMMENT = '情報_ラベル連携テーブル' DEFAULT CHARACTER SET utf8;
 
 
+CREATE TABLE labels_subjects
+(
+	id int unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT 'ラベル_主体連携id',
+	-- モノID
+	subject_id int unsigned NOT NULL COMMENT '主体id : モノID',
+	-- タグID
+	label_id int unsigned NOT NULL COMMENT 'ラベルid : タグID',
+	-- 作成日時
+	created datetime COMMENT '作成日時 : 作成日時',
+	-- 変更日時
+	modified datetime COMMENT '変更日時 : 変更日時',
+	PRIMARY KEY (id)
+) ENGINE = InnoDB COMMENT = 'ラベル_主体連携テーブル' DEFAULT CHARACTER SET utf8;
+
+
 
 /* Create Foreign Keys */
+
+ALTER TABLE informations_subjects
+	ADD FOREIGN KEY (subject_id)
+	REFERENCES subjects (id)
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
+;
+
 
 ALTER TABLE labels_subjects
 	ADD FOREIGN KEY (subject_id)
@@ -116,14 +124,6 @@ ALTER TABLE labels_subjects
 ;
 
 
-ALTER TABLE informations_subjects
-	ADD FOREIGN KEY (subject_id)
-	REFERENCES subjects (id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
 ALTER TABLE informations_labels
 	ADD FOREIGN KEY (information_id)
 	REFERENCES informations (id)
@@ -140,7 +140,7 @@ ALTER TABLE informations_subjects
 ;
 
 
-ALTER TABLE informations_labels
+ALTER TABLE labels_subjects
 	ADD FOREIGN KEY (label_id)
 	REFERENCES labels (id)
 	ON UPDATE RESTRICT
@@ -148,7 +148,7 @@ ALTER TABLE informations_labels
 ;
 
 
-ALTER TABLE labels_subjects
+ALTER TABLE informations_labels
 	ADD FOREIGN KEY (label_id)
 	REFERENCES labels (id)
 	ON UPDATE RESTRICT
