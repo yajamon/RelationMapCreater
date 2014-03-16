486a797554cafa0f1623bc3d7d813bf5c4ce9e53
yajamon <yajamon.tatsuki@gmail.com>
2014-03-16 14:47:30 +0900

    edit database information

--- src/database/relationMapCreater.sql
+++ src/database/relationMapCreater.sql
@@ -2,46 +2,46 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 /* Drop Tables */
 
-DROP TABLE objects_tags;
-DROP TABLE informations_objects;
-DROP TABLE objects;
-DROP TABLE informations_tags;
+DROP TABLE informations_subjects;
+DROP TABLE subjects_labels;
+DROP TABLE subjects;
+DROP TABLE informations_labels;
 DROP TABLE informations;
-DROP TABLE tags;
+DROP TABLE labels;
 
 
 
 
 /* Create Tables */
 
-CREATE TABLE objects
+CREATE TABLE subjects
 (
 	-- モノID
-	id int unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT 'モノid : モノID',
+	id int unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT '主体id : モノID',
 	-- モノの名称
-	name varchar(255) NOT NULL COMMENT 'モノ名称 : モノの名称',
+	name varchar(255) NOT NULL COMMENT '主体名称 : モノの名称',
 	-- 作成日時
 	created datetime COMMENT '作成日時 : 作成日時',
 	-- 変更日時
 	modified datetime COMMENT '変更日時 : 変更日時',
 	PRIMARY KEY (id)
-) ENGINE = InnoDB COMMENT = 'モノテーブル' DEFAULT CHARACTER SET utf8;
+) ENGINE = InnoDB COMMENT = '主体テーブル' DEFAULT CHARACTER SET utf8;
 
 
-CREATE TABLE informations_objects
+CREATE TABLE informations_subjects
 (
 	-- 識別id
 	id int unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT '情報_モノ連携id : 識別id',
 	-- 情報id
 	information_id int unsigned NOT NULL COMMENT '情報id : 情報id',
 	-- モノID
-	object_id int unsigned NOT NULL COMMENT 'モノid : モノID',
+	subject_id int unsigned NOT NULL COMMENT '主体id : モノID',
 	-- 作成日時
 	created datetime COMMENT '作成日時 : 作成日時',
 	-- 変更日時
 	modified datetime COMMENT '変更日時 : 変更日時',
 	PRIMARY KEY (id)
-) ENGINE = InnoDB COMMENT = '情報_モノ連携テーブル' DEFAULT CHARACTER SET utf8;
+) ENGINE = InnoDB COMMENT = '情報_主体連携テーブル' DEFAULT CHARACTER SET utf8;
 
 
 -- 情報テーブル
@@ -61,70 +61,70 @@ CREATE TABLE informations
 ) ENGINE = InnoDB COMMENT = '情報テーブル : 情報テーブル' DEFAULT CHARACTER SET utf8;
 
 
-CREATE TABLE informations_tags
+CREATE TABLE informations_labels
 (
-	id int unsigned NOT NULL UNIQUE COMMENT '情報_タグid',
+	id int unsigned NOT NULL UNIQUE COMMENT '情報_ラベルid',
 	-- 情報id
 	information_id int unsigned NOT NULL COMMENT '情報id : 情報id',
 	-- タグID
-	tag_id int unsigned NOT NULL COMMENT 'タグid : タグID',
+	label_id int unsigned NOT NULL COMMENT 'ラベルid : タグID',
 	-- 作成日時
 	created datetime COMMENT '作成日時 : 作成日時',
 	-- 変更日時
 	modified datetime COMMENT '変更日時 : 変更日時',
 	PRIMARY KEY (id)
-) ENGINE = InnoDB COMMENT = '情報_タグ連携テーブル' DEFAULT CHARACTER SET utf8;
+) ENGINE = InnoDB COMMENT = '情報_ラベル連携テーブル' DEFAULT CHARACTER SET utf8;
 
 
-CREATE TABLE objects_tags
+CREATE TABLE subjects_labels
 (
-	id int unsigned NOT NULL UNIQUE COMMENT 'モノ_タグ連携id',
+	id int unsigned NOT NULL UNIQUE COMMENT '主体_タグ連携id',
 	-- モノID
-	object_id int unsigned NOT NULL COMMENT 'モノid : モノID',
+	subject_id int unsigned NOT NULL COMMENT '主体id : モノID',
 	-- タグID
-	tag_id int unsigned NOT NULL COMMENT 'タグid : タグID',
+	label_id int unsigned NOT NULL COMMENT 'ラベルid : タグID',
 	-- 作成日時
 	created datetime COMMENT '作成日時 : 作成日時',
 	-- 変更日時
 	modified datetime COMMENT '変更日時 : 変更日時',
 	PRIMARY KEY (id)
-) ENGINE = InnoDB COMMENT = 'モノ_タグ連携テーブル' DEFAULT CHARACTER SET utf8;
+) ENGINE = InnoDB COMMENT = '主体_ラベル連携テーブル' DEFAULT CHARACTER SET utf8;
 
 
-CREATE TABLE tags
+CREATE TABLE labels
 (
 	-- タグID
-	id int unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT 'タグid : タグID',
+	id int unsigned NOT NULL UNIQUE AUTO_INCREMENT COMMENT 'ラベルid : タグID',
 	-- タグの名称
-	name varchar(255) NOT NULL COMMENT 'タグ名称 : タグの名称',
+	name varchar(255) NOT NULL COMMENT 'ラベル名称 : タグの名称',
 	-- 作成日時
 	created datetime COMMENT '作成日時 : 作成日時',
 	-- 変更日時
 	modified datetime COMMENT '変更日時 : 変更日時',
 	PRIMARY KEY (id)
-) ENGINE = InnoDB COMMENT = 'タグテーブル' DEFAULT CHARACTER SET utf8;
+) ENGINE = InnoDB COMMENT = 'ラベルテーブル' DEFAULT CHARACTER SET utf8;
 
 
 
 /* Create Foreign Keys */
 
-ALTER TABLE objects_tags
-	ADD FOREIGN KEY (object_id)
-	REFERENCES objects (id)
+ALTER TABLE informations_subjects
+	ADD FOREIGN KEY (subject_id)
+	REFERENCES subjects (id)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
 
 
-ALTER TABLE informations_objects
-	ADD FOREIGN KEY (object_id)
-	REFERENCES objects (id)
+ALTER TABLE subjects_labels
+	ADD FOREIGN KEY (subject_id)
+	REFERENCES subjects (id)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
 
 
-ALTER TABLE informations_tags
+ALTER TABLE informations_subjects
 	ADD FOREIGN KEY (information_id)
 	REFERENCES informations (id)
 	ON UPDATE RESTRICT
@@ -132,7 +132,7 @@ ALTER TABLE informations_tags
 ;
 
 
-ALTER TABLE informations_objects
+ALTER TABLE informations_labels
 	ADD FOREIGN KEY (information_id)
 	REFERENCES informations (id)
 	ON UPDATE RESTRICT
@@ -140,17 +140,17 @@ ALTER TABLE informations_objects
 ;
 
 
-ALTER TABLE objects_tags
-	ADD FOREIGN KEY (tag_id)
-	REFERENCES tags (id)
+ALTER TABLE subjects_labels
+	ADD FOREIGN KEY (label_id)
+	REFERENCES labels (id)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
 
 
-ALTER TABLE informations_tags
-	ADD FOREIGN KEY (tag_id)
-	REFERENCES tags (id)
+ALTER TABLE informations_labels
+	ADD FOREIGN KEY (label_id)
+	REFERENCES labels (id)
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
